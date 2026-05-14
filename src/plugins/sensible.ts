79b6973742a8b329143a7bd20ada51262c67fe4b
Gustavo Fantoni <124843952+GustavoFantoni@users.noreply.github.com>
2026-05-14 17:42:32 -0300

    Update sensible.ts

--- src/plugins/sensible.ts
+++ src/plugins/sensible.ts
@@ -1,11 +1,6 @@
 import fp from 'fastify-plugin'
 import sensible from '@fastify/sensible'
 
-/**
- * This plugins adds some utilities to handle http errors
- *
- * @see https://github.com/fastify/fastify-sensible
- */
 export default fp(async (fastify) => {
   fastify.register(sensible)
 })
